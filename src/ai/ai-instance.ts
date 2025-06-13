50453d5b86f4b0e4b9512a08c2a006879fa91268
Mücteba <muctebakoksal@gmail.com>
2025-06-13 11:26:04 +0000

    Bunun modelini dahah kapsamlı olması için gemini-2.5-flash api ile değiş

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -9,9 +9,13 @@ export const ai = genkit({
       apiKey: process.env.GOOGLE_GENAI_API_KEY,
       defaultGenerationOptions: {
         maxOutputTokens: 8192, 
-        topP: 0.95, 
+        topP: 0.95,
+        thinkingConfig: {
+          thinkingBudget: 24576,
+        },
       },
     }),
   ],
-  model: 'googleai/gemini-2.0-flash-exp',
+  model: 'googleai/gemini-2.5-flash-preview-05-20',
 });
+
