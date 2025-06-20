eb4317eea47b1c476a56fb1e83cea0c70930d0d0
Mücteba <muctebakoksal@gmail.com>
2025-06-20 08:55:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,26 @@ export default function RootLayout({
           <Toaster /> {/* Add Toaster component */}
         </AppProviders>
         <script async src="https://quintessentialreport.com/b/3/V/0/P.3ypsv/b/mFVJJfZ/D/0p2yNsD/MD5hMpzMQp4HLaTwYT0BM/zRk/zwNkDfkw"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(huq){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = huq || {};
+s.src = "//quintessentialreport.com/c.Db9L6-b/2/5rlWSEWgQa9nNLjZQAzNOzTHMd0hODC-0k2/NvDjMj5UM/zsQa5M";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+if (l && l.parentNode) {
+  l.parentNode.insertBefore(s, l);
+} else {
+  document.body.appendChild(s); // Fallback if 'l' is not found
+}
+})({})
+            `,
+          }}
+        />
       </body>
     </html>
   );
